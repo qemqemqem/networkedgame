496abbd5aa9e7248c3534ce9c4733accc215daf2
csheldondante <csheldondante@gmail.com>
2022-09-25 09:17:54 -0400

    added in controls and camera and targeting for gamepad and keyboard and mouse

---
+++ Assets/BulidingComponent.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulidingComponent : MonoBehaviour
+{
+    public Faction faction;
+    public Renderer bulidingRenderer;
+    public string unitType;
+    public float spawnRate=.1f;
+    public float timeSinceSpawn = 0f;
+    public TMPro.TextMeshPro textMesh;
+    public int garrisonCount=0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnSpawn(Faction faction)
+    {
+        this.faction = faction;
+        bulidingRenderer.material = faction.unitMaterial;
+        textMesh.color = faction.factionColor;
+    }
+
+    public void UpdateCount(int garrisonCount)
+    {
+        this.garrisonCount = garrisonCount;
+
+        textMesh.text = "" + this.garrisonCount;
+    }
+}
---
+++ Assets/Leader.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Leader : MonoBehaviour
+{
+    public bool isPc=false;
+    private Dictionary<string, UnitGroup> units = new Dictionary<string, UnitGroup>();
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void AddUnit(Unit unit)
+    {
+        UnitGroup unitGroup;
+        if (!units.TryGetValue(unit.unitType, out unitGroup)) {
+            unitGroup = new UnitGroup();
+            units.Add(unit.unitType, unitGroup);
+        }
+        unitGroup.units.Add(unit);
+    }
+
+    public UnitGroup GetUnitGroup(string unitType)
+    {
+        UnitGroup unitGroup;
+        if (units.TryGetValue(unitType, out unitGroup))
+            return unitGroup;
+        return null;
+    }
+}
